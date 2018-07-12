6c412a73ddcf8f070dd16c6fc08910a1528ecd00
Paul Welle <pdgwelle@gmail.com>
2018-07-12 09:33:44 -0500

    Let's make our y's follow some trend

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import numpy as np
 import fmj
 
 ### Generate some data
-X = np.random.rand(1000,10)
-y = np.random.binomial(n=1, p=0.5, size=1000)
+X = np.random.rand(1000,10) - 0.5
+z_func = lambda x: -0.1 + 0.4*x[1] - 0.6*x[3] + 0.2*x[5] - 0.4*x[7] + \
+    0.1*x[9] - 0.2*x[0]*x[2] + 0.4*x[4]*x[6] - 0.3*x[8]**2
+z = np.apply_along_axis(z_func, axis=1, arr=X)
+p = 1 / (1 + np.exp(-z))
+y = np.array([0]*len(p))
+y[p>0.5] = 1
 
 ### Run classifer
 junior = fmj.FlashMobJunior()
 junior.fit(X,y)
-junior.predict_proba(X)
+f = junior.predict(X, threshold=0.5)
